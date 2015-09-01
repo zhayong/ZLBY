6ddf42ddb6d203dd687fcb7b2119d855c85669c1
zhayong <zhayong@AccenturedeMac-mini.local>
2015-09-01 14:45:10 +0800

    first commit

---
+++ ZLBY/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLBY
+//
+//  Created by zhayong on 15/9/1.
+//  Copyright (c) 2015年 zhayong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZLBY/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZLBY
+//
+//  Created by zhayong on 15/9/1.
+//  Copyright (c) 2015年 zhayong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
