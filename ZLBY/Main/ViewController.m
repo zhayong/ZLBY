6832b24876cfe17d1e186d74a7a1cbb1353ce672
zhayong <zhayong@AccenturedeMac-mini.local>
2015-09-01 15:54:52 +0800

    origin

--- ZLBY/Main/ViewController.m
+++ ZLBY/Main/ViewController.m
@@ -16,8 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"hello");
-    NSLog(@"hello Stephen");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
